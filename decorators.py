26a7e2b8f66580bc76ee02a4e25529be9a49902a
agustinservin1 <agustinservin199103@gmail.com>
2024-12-16 00:04:06 -0300

    Add decorator: calculate_costs

--- decorators.py
+++ decorators.py
@@ -13,3 +13,11 @@ def validate_address(func):
         return func(*args, **kwargs)
     return wrapper
 
+def calculate_costs(func): 
+    def wrapper(*args, **kwargs):
+         distance = kwargs.get('distance', 1)
+         weight = kwargs.get('weight', 1) 
+         cost = distance * weight * 0.5
+         result = func(*args, **kwargs)
+         return f"{result} | Estimated cost: ${cost:.2f}"
+    return wrapper 
